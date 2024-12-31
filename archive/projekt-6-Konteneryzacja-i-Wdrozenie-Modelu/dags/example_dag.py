18979a77244f9b7505ea21a490ad0eb0514d4df3
s23756 <s23756@pjwstk.edu.pl>
2024-12-31 22:26:54 +0100

    Projekt-7-Publikacja-Modelu

--- archive/projekt-6-Konteneryzacja-i-Wdrozenie-Modelu/dags/example_dag.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
-
-# Definicja DAG
-with DAG('example_dag', start_date=datetime(2024, 1, 1), schedule_interval='@daily') as dag:
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
-    start >> end
